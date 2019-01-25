2b90db8e16f543eed3ae55e813b0acd4c00475f6
jillwilliams <codeGeek.03.14@gmail.com>
2019-01-25 00:25:21 -0700

    Jan. 25

---
+++ src/data.js
@@ -0,0 +1,125 @@
+export const storeProducts = [{
+    id: 1,
+    title: "Google Pixel - Black",
+    img: "img/product-1.png",
+    price: 10,
+    company: "GOOGLE",
+    info: "BLAH, BLAH",
+    inCart: false,
+    count: 0,
+    total: 0
+},
+{
+    id: 2,
+    title: "Samsung S7",
+    img: "img/product-2.png",
+    price: 16,
+    company: "SAMSUNG",
+    info: "BLAH BLAH BLAH",
+    inCart: false,
+    count: 0,
+    total: 0
+},
+{
+    id: 3,
+    title: "Samsung Galaxy Note 4",
+    img: "img/product-3.png",
+    price: 35,
+    company: "SAMSUNG",
+    info: "BLAH BLAH BLAH",
+    inCart: false,
+    count: 0,
+    total: 0
+},
+{
+    id: 4,
+    title: "Iphone1",
+    img: "img/product-4.png",
+    price: 10,
+    company: "Apple",
+    info: "BLAH BLAH BLAH",
+    inCart: false,
+    count: 0,
+    total: 0
+},
+{
+    id: 5,
+    title: "Samsung 2",
+    img: "img/product-5.png",
+    price: 5,
+    company: "SAMSUNG",
+    info: "BLAH BLAH BLAH",
+    inCart: false,
+    count: 0,
+    total: 0
+},
+{
+    id: 6,
+    title: "Iphone2",
+    img: "img/product-6.png",
+    price: 15,
+    company: "Apple",
+    info: "BLAH BLAH BLAH",
+    inCart: false,
+    count: 0,
+    total: 0
+},
+{
+    id: 7,
+    title: "Blackberry",
+    img: "img/product-7.png",
+    price: 1,
+    company: "Motorola",
+    info: "BLAH BLAH BLAH",
+    inCart: false,
+    count: 0,
+    total: 0
+},
+{
+    id: 8,
+    title: "Nook Tablet",
+    img: "img/product-8.png",
+    price: 10,
+    company: "Barnes and Noble",
+    info: "BLAH BLAH BLAH",
+    inCart: false,
+    count: 0,
+    total: 0
+},
+{
+    id: 9,
+    title: "TeslaM1",
+    img: "img/product-9.png",
+    price: 200,
+    company: "Tesla",
+    info: "BLAH BLAH BLAH",
+    inCart: false,
+    count: 0,
+    total: 0
+},
+{
+    id: 10,
+    title: "Galaxy Note5",
+    img: "img/product-10.png",
+    price: 25,
+    company: "SAMSUNG",
+    info: "BLAH BLAH BLAH",
+    inCart: false,
+    count: 0,
+    total: 0
+},
+];
+
+export const detailProduct = {
+    id: 1,
+    title: "Google Pixel - Black",
+    img: "img/product-1.png",
+    price: 10,
+    company: "Google",
+    info: "BLAH BLAH BLAH",
+    incart: false,
+    count: 0,
+    total: 0
+};
+
+
